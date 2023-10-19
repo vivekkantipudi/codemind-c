877a89050da242c1919a15f945afcffa658eb421
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2023-10-19 10:14:42 +0530

    #include<stdio.h> int main() {     int X,c;     scanf("%d",&X);     c=X*4;     if(c<=1000)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Tickets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X,c;
+    scanf("%d",&X);
+    c=X*4;
+    if(c<=1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
