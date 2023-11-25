2a1c4ce10a40a82181959df36b416758b3997822
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2023-11-25 19:37:03 +0530

    #include<stdio.h> int main() {     int i,n,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {        if(n%i==0)         {             c=c+1;         }     }     if(c==2)     {         printf("Prime");     }     else     {         printf("Not Prime");     } }

--- Prime.c
+++ Prime.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,c=0;
+    int i,n,c=0;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        if(n%i==0)
+       if(n%i==0)
         {
             c=c+1;
         }
@@ -14,7 +14,7 @@ int main()
     {
         printf("Prime");
     }
-    else 
+    else
     {
         printf("Not Prime");
     }
