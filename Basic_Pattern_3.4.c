2023c80f5c7aed263b4647914bc693445df9aa5b
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2023-12-16 12:07:47 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=64+n;i>=65;i--)     {         for(j=64+n;j>=65;j--)         {             if(i>=j)             {                 printf("%c ",i);             }         }         printf(" ");     } }

---
+++ Basic_Pattern_3.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=64+n;i>=65;i--)
+    {
+        for(j=64+n;j>=65;j--)
+        {
+            if(i>=j)
+            {
+                printf("%c ",i);
+            }
+        }
+        printf("
+");
+    }
+}
